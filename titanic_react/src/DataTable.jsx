800f8498b5ce0e5e79beac9b445ed0bd3abe57dc
dimitri-hoareau <dimitrihoareau2@gmail.com>
2024-04-03 09:27:55 +0200

    add filter to display or add columns

--- titanic_react/src/DataTable.jsx
+++ titanic_react/src/DataTable.jsx
@@ -1,44 +1,85 @@
-import React from 'react';
-import './DataTable.css'; // Assurez-vous de créer ce fichier CSS pour le style
+import React, { useState } from 'react';
+import './DataTable.css'; 
 
 const DataTable = ({ data }) => {
+
+  const [visibleColumns, setVisibleColumns] = useState({
+    PassengerId: true,
+    Survived: true,
+    Pclass: true,
+    Name: true,
+    Sex: true,
+    Age: true,
+    SibSp: true,
+    Parch: true,
+    Ticket: true,
+    Fare: true,
+    Cabin : true,
+    Embarked: true,
+  });
+
+  const toggleColumn = (column) => {
+    setVisibleColumns({
+      ...visibleColumns,
+      [column]: !visibleColumns[column],
+    });
+  };
+
+  
   return (
+  <>
+    <div className="column-controls">
+      {Object.keys(visibleColumns).map((column, index) => (
+        <label key={index}>
+          <input
+            type="checkbox"
+            checked={visibleColumns[column]}
+            onChange={() => toggleColumn(column)}
+          />
+          {column}
+        </label>
+      ))}
+    </div>
+    
+    
+    
     <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Survived</th>
-          <th>Class</th>
-          <th>Name</th>
-          <th>Sex</th>
-          <th>Age</th>
-          <th>Siblings/Spouses</th>
-          <th>Parents/Children</th>
-          <th>Ticket</th>
-          <th>Fare</th>
-          <th>Cabin</th>
-          <th>Embarked</th>
+    <thead>
+      <tr>
+        {visibleColumns.PassengerId && <th>ID</th>}
+        {visibleColumns.Survived && <th>Survived</th>}
+        {visibleColumns.Pclass && <th>Class</th>}
+        {visibleColumns.Name && <th>Name</th>}
+        {visibleColumns.Sex && <th>Sex</th>}
+        {visibleColumns.Age && <th>Age</th>}
+        {visibleColumns.SibSp && <th>Siblings/Spouses</th>}
+        {visibleColumns.Parch && <th>Parents/Children</th>}
+        {visibleColumns.Ticket && <th>Ticket</th>}
+        {visibleColumns.Fare && <th>Fare</th>}
+        {visibleColumns.Cabin && <th>Cabin</th>}
+        {visibleColumns.Embarked && <th>Embarked</th>}
+      </tr>
+    </thead>
+    <tbody>
+      {data.map((item, index) => (
+        <tr key={index}>
+          {visibleColumns.PassengerId && <td>{item.PassengerId}</td>}
+          {visibleColumns.Survived && <td>{item.Survived ? 'Yes' : 'No'}</td>}
+          {visibleColumns.Pclass && <td>{item.Pclass}</td>}
+          {visibleColumns.Name && <td>{item.Name}</td>}
+          {visibleColumns.Sex && <td>{item.Sex}</td>}
+          {visibleColumns.Age && <td>{item.Age}</td>}
+          {visibleColumns.SibSp && <td>{item.SibSp}</td>}
+          {visibleColumns.Parch && <td>{item.Parch}</td>}
+          {visibleColumns.Ticket && <td>{item.Ticket}</td>}
+          {visibleColumns.Fare && <td>{item.Fare}</td>}
+          {visibleColumns.Cabin && <td>{item.Cabin || 'N/A'}</td>}
+          {visibleColumns.Embarked && <td>{item.Embarked}</td>}
         </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            <td>{item.PassengerId}</td>
-            <td>{item.Survived ? 'Yes' : 'No'}</td>
-            <td>{item.Pclass}</td>
-            <td>{item.Name}</td>
-            <td>{item.Sex}</td>
-            <td>{item.Age}</td>
-            <td>{item.SibSp}</td>
-            <td>{item.Parch}</td>
-            <td>{item.Ticket}</td>
-            <td>{item.Fare}</td>
-            <td>{item.Cabin || 'N/A'}</td>
-            <td>{item.Embarked}</td>
-          </tr>
-        ))}
-      </tbody>
+      ))}
+    </tbody>
     </table>
+    </>
   );
 };
 
