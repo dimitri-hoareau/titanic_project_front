fb50d844913e87165751f3ef7d4345bc77ddd405
dimitri-hoareau <dimitrihoareau2@gmail.com>
2024-04-03 09:35:29 +0200

    fetch real data from API

--- titanic_react/src/App.jsx
+++ titanic_react/src/App.jsx
@@ -1,14 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import DataTable from './DataTable'
 import {fakeData} from './fakeData.js'
 import './App.css'
 
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/passengers/')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []); 
   
+  console.log(data)
 
   return (
     <>
-  <DataTable data={fakeData} />
+  <DataTable data={data} />
     </>
   )
 }
--- titanic_react/src/DataTable.jsx
+++ titanic_react/src/DataTable.jsx
@@ -41,8 +41,6 @@ const DataTable = ({ data }) => {
       ))}
     </div>
     
-    
-    
     <table>
     <thead>
       <tr>
